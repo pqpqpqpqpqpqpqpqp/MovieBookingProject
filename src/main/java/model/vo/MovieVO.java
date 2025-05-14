1e02a0f22cd84244b38d530d2e90a86d0ba8abd3
NHD <damitup.00@gmail.com>
2025-05-14 17:11:45 +0900

    moiveVO 추가

---
+++ src/main/java/model/vo/MovieVO.java
@@ -0,0 +1,73 @@
+package model.vo;
+
+public class MovieVO {
+	
+	private int movieIdx;
+	private String movieName;
+	private String movieDsec;
+	private String movieOpenDate;
+	private String movieCloseDate;
+	private String movieCreator;
+	private String movieAgeGrade;
+	private String movieImg;
+	private String moviePlayTime;
+	
+	public MovieVO() {
+		
+	}
+	
+	public int getMovieIdx() {
+		return movieIdx;
+	}
+	public void setMovieIdx(int movieIdx) {
+		this.movieIdx = movieIdx;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public String getMovieDsec() {
+		return movieDsec;
+	}
+	public void setMovieDsec(String movieDsec) {
+		this.movieDsec = movieDsec;
+	}
+	public String getMovieOpenDate() {
+		return movieOpenDate;
+	}
+	public void setMovieOpenDate(String movieOpenDate) {
+		this.movieOpenDate = movieOpenDate;
+	}
+	public String getMovieCloseDate() {
+		return movieCloseDate;
+	}
+	public void setMovieCloseDate(String movieCloseDate) {
+		this.movieCloseDate = movieCloseDate;
+	}
+	public String getMovieCreator() {
+		return movieCreator;
+	}
+	public void setMovieCreator(String movieCreator) {
+		this.movieCreator = movieCreator;
+	}
+	public String getMovieAgeGrade() {
+		return movieAgeGrade;
+	}
+	public void setMovieAgeGrade(String movieAgeGrade) {
+		this.movieAgeGrade = movieAgeGrade;
+	}
+	public String getMovieImg() {
+		return movieImg;
+	}
+	public void setMovieImg(String movieImg) {
+		this.movieImg = movieImg;
+	}
+	public String getMoviePlayTime() {
+		return moviePlayTime;
+	}
+	public void setMoviePlayTime(String moviePlayTime) {
+		this.moviePlayTime = moviePlayTime;
+	}
+}
