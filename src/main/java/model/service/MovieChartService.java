cd69562ee658798083271f8c221b0c86855ce5dc
NHD <damitup.00@gmail.com>
2025-05-16 15:12:45 +0900

    MovieChartService - 생성 (action에서 service로 명칭 변경)

---
+++ src/main/java/model/service/MovieChartService.java
@@ -0,0 +1,9 @@
+package model.service;
+
+import model.dao.MovieChartDAO;
+import model.vo.MovieChartVO;
+
+public class MovieChartService {
+	
+	MovieChartDAO moviechartDAO = new MovieChartDAO();
+}
