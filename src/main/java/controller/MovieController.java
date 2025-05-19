b1a85b1cf5ca47d57bd496609a99445512492377
NHD <damitup.00@gmail.com>
2025-05-19 10:55:42 +0900

    Feat: movie web controller 추가

--- src/main/java/controller/MovieController.java
+++ src/main/java/controller/MovieController.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
+import model.service.ViewMainHomeService;
 import movie.service.MovieUserListService;
 import util.ResponseData;
 
 public class MovieController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -29,24 +30,32 @@ public class MovieController extends HttpServlet {
 	protected void doPocess(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String url = request.getRequestURI(); // /서버path/url(mapping)
-		String path = request.getContextPath();// /서버path
-		String command = url.substring(path.length()); // /url(mapping)
-		System.out.println("api호출: " + command);
-		
-		Gson gson = new Gson();
-		ResponseData responseData = null;
-		
-		if(command.equals("/MovieChart.mo")) {
-			MovieUserListService service = new MovieUserListService();
-			responseData = service.execute(request, response);
-		}
+		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		response.setContentType("application/json; charset=UTF-8");
+
+		String url = request.getRequestURI();
+		String path = request.getContextPath();
+		String command = url.substring(path.length());
 		
-		PrintWriter out = response.getWriter();
-		out.print(gson.toJson(responseData));
-		out.flush();
+		System.out.println(url);
+
+		ActionForward forward = null;
+
+		System.out.println("MovieController: "+command);
+
+		if (url.endsWith("mainHome.mow")) {
+			//forward = new ActionForward();
+			//forward.setPath("mainHome.jsp");
+			//forward.setRedirect(false);
+		}
+
+
+		if (forward.isRedirect()) {
+			response.sendRedirect(forward.getPath());
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+			dispatcher.forward(request, response);
+		}
 	}
-	
+
 }
