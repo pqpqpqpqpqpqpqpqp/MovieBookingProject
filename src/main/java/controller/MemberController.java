487a4f51c37a6fa8b8ba06d47bfd0fc037624bb3
JMK <jjsteel8@naver.com>
2025-05-28 17:46:23 +0900

    -- memberController 머지 충돌 에러 해결

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,64 +9,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.google.gson.Gson;
-
-import member.vo.MemberVO;
 
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPocess(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPocess(request, response);
 	}
 
-	protected void doPocess(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	
+	protected void doPocess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = request.getRequestURI();
 		String path = request.getContextPath();
 		String command = url.substring(path.length());
-
-		System.out.println("web이동: " + command);
+		
+		System.out.println("web이동: "+command);
+		
 
 		String forward = null;
 		if (command.equals("/userLogin.me")) {
 			forward = "/view/member/userLogin.jsp";
-		} else if (command.equals("/userRegister.me")) {
+		} else if(command.equals("/userRegister.me")) {
 			forward = "/view/member/userRegister.jsp";
-		} else if (command.equals("/main.me")) {
+		} else if(command.equals("/main.me")) {
 			forward = "/view/main.jsp";
-		} else if (command.equals("/logout.me")) {
+		} else if(command.equals("/logout.me")) {
 			HttpSession session = request.getSession();
 			session.invalidate();
 			forward = "/view/main.jsp";
-		} else if (command.equals("/myPagemain.me")) {
-			forward = "/view/member/myPagemain.jsp";
-		} else if (command.equals("/myCgvHome.me")) {
-			forward = "/view/member/userPage/myTabPage/myCgvHome.jsp";
-		} else if (command.equals("/myTicket.me")) {
-			forward = "/view/member/userPage/myTabPage/myTicket.jsp";
-		} else if (command.equals("/userInfo.me")) {
-			forward = "/view/member/userPage/myTabPage/userInfo.jsp";
-		} else if (command.equals("/userSelectInfo.me")) {
-			forward = "/view/member/userPage/myTabPage/userSelectInfo.jsp";
-		} else if (command.equals("/userDelete.me")) {
-			forward = "/view/member/userPage/myTabPage/userDelete.jsp";
-		} else if (command.equals("/editName.me")) {
-			forward = "/view/member/userPage/myTabPage/userInfoSubPage/editName.jsp";
-		} else if (command.equals("/editPhone.me")) {
-			forward = "/view/member/userPage/myTabPage/userInfoSubPage/editPhone.jsp";
-		} else if (command.equals("/editPassword.me")) {
-			forward = "/view/member/userPage/myTabPage/userInfoSubPage/editPassword.jsp";
-		}
-
+		} 
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(forward);
 		dispatcher.forward(request, response);
-
+		
 	}
 }
